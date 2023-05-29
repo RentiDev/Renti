52e598d1ab60d7f89db288a9c5c278516fba9faf
Nicolas Liu <nicolasliu18@gmail.com>
2023-05-29 12:29:05 -0400

    header bar, linking pages, layout w header and footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <header className="fixed top-0 left-0 right-0 z-50">
+        <div className="container flex items-center justify-between px-6 py-8 mx-auto max-w-screen-x1">
+          <div className="flex items-center">
+            {/* Logo */}
+            <Link href="/" className="text-black text-3xl font-lufgaExtraBold">
+              Renti
+            </Link>
+          </div>
+            <nav className="flex items-center bg-[hsl(246,246%,246%)] rounded-full p-6">
+                {/* // Navigation Links  */}
+                <nav className="flex space-x-16">
+                <Link href="/" className="text-black font-lufga">
+                    Tenant
+                </Link>
+                <Link href="/" className="text-black font-lufga">
+                    Landlord
+                </Link>
+                <Link href="/" className="text-black font-lufga">
+                    Listings
+                </Link>
+                <Link href="/about" className="text-black font-lufga">
+                    About
+                </Link>
+                <Link href="/" className="text-black font-lufga">
+                    Contact Us
+                </Link>
+                {/* // Add more links as needed  */}
+                </nav>
+                {/* // Sign up button  */}
+            </nav> 
+            <Link href="/" className="px-4 py-2 ml-4 text-black font-lufga">
+                Sign Up
+            </Link>
+        </div>
+      </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
