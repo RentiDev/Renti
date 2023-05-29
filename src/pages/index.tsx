367614f64c223efcf3f5db799df6c60111dcc6e0
Nicolas Liu <nicolasliu18@gmail.com>
2023-05-29 01:25:30 -0400

    lufga font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-5xl font-lufgaExtraBold tracking-tight text-white sm:text-[5rem]">
             Renting <span className="text-[hsl(280,100%,70%)]">Reimagined</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-lufga">First Steps →</h3>
               <div className="text-lg">
                 Just the basics - Everything you need to know to set up your
                 database and authentication.
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <h3 className="text-2xl font-lufgaBold">Documentation →</h3>
               <div className="text-lg">
                 Learn more about Create T3 App, the libraries it uses, and how
                 to deploy it.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lufga: ["Lufga", 'sans-serif'],
+        lufgaBold: ["Lufga-Bold", 'sans-serif'],
+        lufgaLight: ["Lufga-Light", 'sans-serif'],
+        lufgaMedium: ["Lufga-Medium", 'sans-serif'],
+        lufgaSemiBold: ["Lufga-SemiBold", 'sans-serif'],
+        lufgaThin: ["Lufga-Thin", 'sans-serif'],
+        lufgaExtraBold: ["Lufga-ExtraBold", 'sans-serif']
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
