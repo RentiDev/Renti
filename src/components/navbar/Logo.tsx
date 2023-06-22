d84c1f105c6ebabf4c2bee1d7b276a9698bed3a7
Nicolas Liu <nicolasliu18@gmail.com>
2023-06-22 11:29:40 -0400

    ts errors in Navbar, Avatar

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import Image from "next/image";
+// import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 const Logo = () => {
-    const router = useRouter();
+    // const router = useRouter();
 
     return (
         <div className="block cursor-pointer text-4xl font-lufgaBold text-black">
--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -3,7 +3,14 @@ import React, { useState } from "react";
 import Avatar from "../Avatar";
 import { signOut } from "next-auth/react";
 
-const UserMenu = ({ session, userName }) => {
+import { type Session } from "next-auth";
+
+interface UserMenuProps {
+    session: Session | null;
+    userName: string;
+}
+
+const UserMenu = ({ session, userName }: UserMenuProps) => {
     const [isHovering, setIsHovering] = useState(false);
 
     return (
@@ -29,8 +36,10 @@ const UserMenu = ({ session, userName }) => {
                                     <div className="absolute right-0 w-full bg-white rounded-lg shadow-xl">
                                         <button
                                             className="block rounded-lg w-full h-full px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-                                            onClick={() => { console.log("logout"); signOut()}}
-                                        >
+                                            onClick={() => {
+                                                console.log("logout");
+                                                signOut().then(() => console.log("Signed out successfully")).catch((error) => console.error(error));
+                                              }} >
                                             Log Out
                                         </button>
                                     </div>
