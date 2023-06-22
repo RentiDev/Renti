1fc21a16472ac3835589e47275dd82aa51529766
Nicolas Liu <nicolasliu18@gmail.com>
2023-06-22 02:27:48 -0400

    CENTERED THE LOG OUT BUTTON BENEATH WELCOME MESSAGE BY MATCHING THE WIDTH OF THE DIVS BECAUSE I AM GENUIS AND SAMUEL IS GOAT

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -7,36 +7,34 @@ const UserMenu = ({ session, userName }) => {
     const [isHovering, setIsHovering] = useState(false);
 
     return (
-        <div className="daisyui flex items-center justify-center hover:scale-110 transition-all cursor-pointer">
+        <div className="daisyui flex items-center transition-all cursor-pointer relative">
             <Link href="/login" 
                 className="text-black font-lufgaMedium text-xl flex items-center"
                 onMouseEnter={() => setIsHovering(true)}
                 onMouseLeave={() => setIsHovering(false)}>
                 { session ? ( 
                     <>
-                        <div className="flex items-center align-middle">
+                        <div className="relative">
                             <div className="p-2">
                                 <span className="flex items-center gap-3"> Welcome, {userName} <Avatar/> </span>
                             </div>
-                            <div className="relative inline-block mt-6">
-                                <div className="relative">
-                                    <span
-                                        className="text-gray-700 cursor-pointer"
-                                        onMouseEnter={() => { setIsHovering(true) }}
-                                        onMouseLeave={() => { setIsHovering(false) }}
-                                    >
-                                    </span>
-                                    { isHovering && (
-                                        <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-xl">
-                                            <button
-                                                className="block rounded-lg w-full h-full px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
-                                                onClick={() => { console.log("logout"); signOut()}}
-                                            >
-                                                Log Out
-                                            </button>
-                                        </div>
-                                    )}
-                                </div>
+                            <div>
+                                <span
+                                    className="text-gray-700 cursor-pointer"
+                                    onMouseEnter={() => { setIsHovering(true) }}
+                                    onMouseLeave={() => { setIsHovering(false) }}
+                                >
+                                </span>
+                                { isHovering && (
+                                    <div className="absolute right-0 w-full bg-white rounded-lg shadow-xl">
+                                        <button
+                                            className="block rounded-lg w-full h-full px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
+                                            onClick={() => { console.log("logout"); signOut()}}
+                                        >
+                                            Log Out
+                                        </button>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </>
